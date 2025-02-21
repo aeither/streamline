033c612396d616475f4b9f4e554a8c3a5e0a862e
aeither <36173828+aeither@users.noreply.github.com>
2025-02-21 01:04:57 +0100

    test create and run graphql works

--- scripts/db/ask-db.ts
+++ scripts/db/ask-db.ts
@@ -1,9 +1,14 @@
 import { extractContextForQuestion } from "../../src/actions/extractContextForQuestion";
+import { getSubgraphQuery } from "../../src/actions/getSubgraphQuery";
 
 async function getFirstContext(question: string) {
     try {
         const context = await extractContextForQuestion(question);
         console.log('First context:', context);
+        if (!context) throw new Error('Could not extract context');
+
+        const query = await getSubgraphQuery(question, context);
+        console.log("🚀 ~ getFirstContext ~ query:", query)
     } catch (error) {
         console.error('Error extracting context:', error);
     }
--- scripts/test-output.ts
+++ scripts/test-output.ts
@@ -1,3 +1,4 @@
+import { createAndRunGraphQL } from "../src/actions/createAndRunGraphQL";
 import { parseUserMessage } from "../src/actions/parseUserMessage";
 
 try {
@@ -5,6 +6,9 @@ try {
     const subgraphUrl = 'https://base-mainnet.subgraph.x.superfluid.dev/';
     const result = await parseUserMessage(input, subgraphUrl);
     console.log('Parsed result:', result);
+
+    const query = await createAndRunGraphQL(result, subgraphUrl);
+    console.log("🚀 ~ query:", query)
 } catch (error) {
     console.error('Error parsing user message:', error);
 }
